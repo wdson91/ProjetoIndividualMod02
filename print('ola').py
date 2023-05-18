82db8374950f3a6c7fa4622eaf4f7268101ceb93
Wdson Danrlei <wdson91@gmail.com>
2023-05-18 12:31:39 -0300

    Update print('ola').py
    adição da função  validarEntrada

--- print('ola').py
+++ print('ola').py
@@ -6,31 +6,46 @@ candidatos = [
     {'nome': 'Carlos', 'resultado': 'e6_t8_p8_s7'}
 ]
 
-
-
-
 numeros = []
 
 for candidato in candidatos:
-    sublista = candidato['resultado'].split('_')  # separa a string em uma lista
+        sublista = candidato['resultado'].split('_')  # separa a string em uma lista
+        
+        numeros_candidato = []
+        for item in sublista:
+            if item[1:].isdigit():  # verifica se o elemento é um número
+                numeros_candidato.append(int(item[1:]))  # converte e adiciona o número à lista
+        numeros.append(numeros_candidato) # criando uma lista somente com as notas de cada candidato
+
+
+def validarEntrada(mensagem):
+        while True:
+            try:
+                entrada = int(input(mensagem))
+                print('-'*60)
+                return entrada
+            except ValueError:
+                print("Por favor, insira um valor inteiro válido.")
     
-    numeros_candidato = []
-    for item in sublista:
-        if item[1:].isdigit():  # verifica se o elemento é um número
-            numeros_candidato.append(int(item[1:]))  # converte e adiciona o número à lista
-    numeros.append(numeros_candidato) # criando uma lista somente com as notas de cada candidato
-
-
+    
+nota1 = validarEntrada("Informe a nota mínima desejada para a entrevista: ")
+nota2 = validarEntrada("Informe a nota mínima desejada para o teste teórico: ")
+nota3 = validarEntrada("Informe a nota mínima desejada para o teste prático: ")
+nota4 = validarEntrada("Informe a nota mínima desejada para as soft skills: ")
 
 def buscarCandidato(nota1,nota2,nota3,nota4):
-    listaResultado = [nota1,nota2,nota3,nota4]
     aprovados = []
-    for i,numero in enumerate(numeros):
-        if listaResultado >=  numeros[i]:
-            aprovados.append(candidatos[i]['nome'])
-        
-    print(f'Candidatos Aprovados:{aprovados}')
+    listaResultado = [nota1,nota2,nota3,nota4]
+    
+    for i,lista in enumerate(numeros):
+        if all(x <= y for x, y in zip(listaResultado, lista)):
+            aprovados.append({candidatos[i]['nome']:lista})
+
+    print('Nenhum candidato aprovado com essas notas') if len(aprovados) <1 else print(f'Candidatos Aprovados:{aprovados}')
+
+
+buscarCandidato(nota1,nota2,nota3,nota4)
 
 
 
-buscarCandidato(4,10,8,8)    
+# buscarCandidato(4,10,8,8)    
